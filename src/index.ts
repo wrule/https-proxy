ce9c05692b4a2594729de7ee3baec19de0730191
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-18 12:08:58 +0800

    http基础代理

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,22 @@ import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 function main() {
-  console.log(1234);
+  const proxyApp = express();
+  const proxyServer = http.createServer(proxyApp);
+
+  proxyApp.use('/', createProxyMiddleware({
+    target: '/',
+    changeOrigin: true,
+    router: (req) => {
+      console.log(req.url);
+      const url = new URL(req.url);
+      const dstRoute = `${url.protocol}//${url.hostname}${url.port ? `:${url.port}` : ''}`;
+      return dstRoute;
+    },
+  }));
+
+  proxyServer.listen(7070);
+  console.log('7070...');
 }
 
 main();
